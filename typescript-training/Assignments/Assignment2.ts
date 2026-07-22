1354addf20652e81fd1729f0c9f5ef66d24b7505
Lokeswara Reddy <lokeswarareddy@itsupports-MacBook-Pro.local>
2026-07-22 18:45:24 +0530

    Assigment2 updated

--- typescript-training/Assignments/Assignment2.ts
+++ typescript-training/Assignments/Assignment2.ts
@@ -4,74 +4,37 @@ let income: number = 5500.0;
 let isEmployed: boolean = true;
 let debtToIncomeRatio: number = 35.0;
 
-// // If the credit score is above 750, the loan is automatically approved.
-// // o If the credit score is between 650 and 750, additional checks are performed.
-// // o If the credit score is below 650, the loan is denied.
+// If the credit score is above 750, the loan is automatically approved.
+// o If the credit score is between 650 and 750, additional checks are performed.
+// o If the credit score is below 650, the loan is denied.
 
-// if (creditScore > 750) {
+if (creditScore > 750) {
 
-//     console.log("Loan is approved")
-
-
-// }
-// else if (creditScore >= 650 && creditScore <= 750) {
-
-//     if (income >= 55000) {
-//         if (isEmployed) {
-//             if (debtToIncomeRatio < 40) {
-
-//                 console.log("loan is approved")
-//             }
-//             else {
-//                 console.log("Loan is denied")
-//             }
-
-//         }
-//         else {
-//             console.log("Loan is denied")
-//         }
-//     }
-//     else{
-
-//          console.log("Loan is denied")
-//     }
-
-// }
-// else {
-
-//     console.log("Loan is denied")
-
-// }
+    console.log("Loan is approved")
 
 
-
-
-if (creditScore >= 650 && creditScore <= 750) {
+}
+else if (creditScore >= 650 && creditScore <= 750) {
 
     if (income >= 55000) {
-
         if (isEmployed && debtToIncomeRatio < 40) {
 
-            console.log("Loan is approved");
+            console.log("loan is approved")
 
         }
         else {
-
-            console.log("Loan is denied");
+            console.log("Loan is denied")
         }
     }
     else {
 
-        console.log("Loan is denied");
+        console.log("Loan is denied")
     }
-
-
-
-
-
 
 }
 else {
 
-    console.log("loan is denied");
+    console.log("Loan is denied")
+
 }
+
