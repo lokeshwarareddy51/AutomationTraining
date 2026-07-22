0421922d100424af5bac231939c9601d19880797
Lokeswara Reddy <lokeswarareddy@itsupports-MacBook-Pro.local>
2026-07-22 18:41:48 +0530

    Assigment1

---
+++ typescript-training/Assignments/Assignment1.ts
@@ -0,0 +1,101 @@
+// 1. Temperature of a city in degrees Celsius: 25.5
+
+let Temperature: number=25.5;
+console.log(`My City Temperature is ${Temperature}`);
+
+// 2. Whether a customer has placed an order: true or false
+
+let isOrderPlaced :boolean=true;
+
+console.log("Yes Order is placed");
+
+// 3. Person's phone number: "123-456-7890"
+
+let PhoneNumber:number=9066981102;
+console.log(`My phone is ${PhoneNumber}`);
+
+// 4. Amount of money in a customer's bank account: 1000.50
+
+let bankBalance:number=1000.50;
+console.log(`My Bank Balance is ${bankBalance}`);
+
+
+// 5. Person's email address: "john.doe@example.com"
+
+let emailAddress:string="lokeshwarareddy51@gmail.com";
+console.log(`My Email id is ${emailAddress}`);
+
+// 6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+
+let latitude:number=37.7749;
+let longitude:number=122.4194;
+
+console.log(`My Lat and Long values are ${latitude} and ${longitude}`);
+
+// 7. Person's marital status: true or false
+
+let ismarried :boolean=true;
+
+console.log("Yes Married");
+
+// 8. Person's occupation: "Software Engineer"
+let occupation : string="software Engineer";
+
+console.log(`My occupation is ${occupation}`);
+
+// 9. Person's favourite colour: "Blue"
+
+let favouriteColor : string="Blue";
+console.log(`My Favourite Color is ${favouriteColor}`);
+
+// 10.Current year: 2023
+
+let currentYear: number=2023;
+console.log(`Current Year is ${currentYear}`);
+
+// 11.Number of followers on a social media platform: 1,000,000
+
+let followers:string = "1,000,000";
+
+console.log(`Number of followers in social media is ${followers}`);
+// 12.Rating of a movie: 7.5
+
+let movieRating:number=7.5;
+console.log(`Movie Rating is ${movieRating}`);
+
+// 13.Person's blood type: 'A'
+
+let bloodGroup:string='A';
+console.log(`My Blood Group is ${bloodGroup}`)
+
+// 14.Title of a book: "To Kill a Mockingbird"
+
+let bookTitle:string="To Kill a Mockingbird";
+console.log(`Book Title is ${bookTitle}`);
+
+// 15.Number of employees in a company: 500
+let noofEmployees:number=500;
+console.log(`Number of Employees is ${noofEmployees}`)
+
+// 16.Time of an event: 2:30 PM
+let eventTime:string="2:30 PM"
+console.log(`Time of Event is ${eventTime}`);
+
+// 17.Name of a country: "United States"
+
+let countryName:string="United States";
+console.log(`Country Name is ${countryName}`);   
+
+// 18.Person's eye color: "Brown"
+
+let eyeColor:String="Brown"
+console.log(`Eye color is ${eyeColor}`);
+
+// 19.Person's birthplace: "New York City"
+
+let birthPlace:string="NewYork City";
+console.log(`Birth Place is ${birthPlace}`);
+
+// 20. Distance between two cities: 200.5
+let distance:number=200.5
+console.log(`Distance between two cities is ${200.5}`);
