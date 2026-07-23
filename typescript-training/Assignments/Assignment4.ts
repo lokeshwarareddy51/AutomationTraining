4ce23944949e9f97472c94885cd581e24373080e
Lokeswara Reddy <lokeswarareddy@itsupports-MacBook-Pro.local>
2026-07-23 09:30:37 +0530

    Assignment4

---
+++ typescript-training/Assignments/Assignment4.ts
@@ -0,0 +1,45 @@
+let transactions: number[] = [50000, -2000, 3000, -15000, -200, -300, 4000, -3000];
+
+let creditcount = 0;
+let debitcount = 0;
+let creditamount = 0;
+let debitamount = 0;
+let remainingAmount = 0;
+
+for (let i = 0; i < transactions.length; i++) {
+
+    if (transactions[i] > 0) {
+
+        creditcount++;
+        creditamount += transactions[i];
+        if (transactions[i] > 10000) {
+
+            console.log(`Suspicious credit Transaction with amount ${transactions[i]}`)
+
+        }
+
+
+
+
+    }
+    else {
+
+        debitcount++;
+        debitamount += transactions[i];
+
+        if (transactions[i] < -10000) {
+
+            console.log(`Suspicious debit Transaction with amount ${transactions[i]}`)
+
+        }
+    }
+
+
+
+}
+remainingAmount = creditamount + debitamount;
+console.log(creditcount);
+console.log(debitcount);
+console.log(creditamount);
+console.log(debitamount);
+console.log(remainingAmount);
